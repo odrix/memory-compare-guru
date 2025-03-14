01bf149e48ab52e33359c00bf4373b0bd502f1f1
Adrien Bey <a.bey@revers.io>
2025-03-14 17:17:30 +0100

    Revert "Enable Server Side Rendering"
    This reverts commit 3200308502efd35360e22d93eaa4ca0f037468c5.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,25 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
-// Create a QueryClient instance that can be used on both server and client
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      // Disable retries on the server
-      retry: false,
-      // Don't refetch on window focus for SSR
-      refetchOnWindowFocus: false,
-    },
-  },
-});
+const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <Routes>
-        <Route path="/" element={<Index />} />
-        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
--- src/main.tsx
+++ src/main.tsx
@@ -1,2 +1,5 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-import './entry-client';
+createRoot(document.getElementById("root")!).render(<App />);
