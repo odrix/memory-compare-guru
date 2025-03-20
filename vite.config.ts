dff856007af9bea7b0279afd27fa4c3bea76ec44
Adrien Bey <a.bey@revers.io>
2025-03-20 18:27:48 +0100

    security

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,12 @@ export default defineConfig(({ command, mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    headers: {
+      "Strict-Transport-Security": "max-age=86400; includeSubDomains", // Adds HSTS options to your website, with a expiry time of 1 day
+      "X-Content-Type-Options": "nosniff", // Protects from improper scripts runnings
+      "X-Frame-Options": "DENY", // Stops your site being used as an iframe
+      "X-XSS-Protection": "1; mode=block", // Gives XSS protection to legacy browsers
+    },
   },
   plugins: [
     react(),
