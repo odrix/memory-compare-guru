99adcb4301aad90e2f0e3faf037ca62c9c40d31f
Adrien Bey <a.bey@revers.io>
2025-03-20 18:21:39 +0100

    reset

--- src/main.tsx
+++ src/main.tsx
@@ -1,2 +1,12 @@
+import React from 'react';
+import { hydrateRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
 
-import './entry-client';
+hydrateRoot(
+  document.getElementById('root') as HTMLElement,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
