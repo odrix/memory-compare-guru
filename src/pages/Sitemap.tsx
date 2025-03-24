98c4f214a51863b5ed349c3feac51d207923753d
Adrien Bey <a.bey@revers.io>
2025-03-24 17:36:49 +0100

    sitemap

---
+++ src/pages/Sitemap.tsx
@@ -0,0 +1,30 @@
+export default function handler(req, res) {
+    // Set the appropriate headers
+    res.setHeader('Content-Type', 'text/xml');
+    res.setHeader('Cache-Control', 'public, max-age=86400, immutable'); // 24 hours
+  
+    // Define your website routes
+    const routes = [
+      { path: '/', priority: '1.0', changefreq: 'daily' },
+      { path: '/type/hdd', priority: '0.8', changefreq: 'weekly' },
+      { path: '/type/nvme', priority: '0.8', changefreq: 'weekly' },
+      { path: '/type/sata', priority: '0.8', changefreq: 'weekly' }
+    ];
+    
+    const domain = 'https://topdisks.eu';
+    const today = new Date().toISOString().split('T')[0];
+    
+    // Generate the XML sitemap
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${routes.map(route => `  <url>
+      <loc>${domain}${route.path}</loc>
+      <lastmod>${today}</lastmod>
+      <changefreq>${route.changefreq}</changefreq>
+      <priority>${route.priority}</priority>
+    </url>`).join('\n')}
+  </urlset>`;
+    
+    // Send the XML response
+    res.status(200).send(sitemap);
+  }
